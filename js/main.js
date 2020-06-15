b4cdab26f31558ba89ad21f973ee17de201b3b98
Xavi Ortega <xavier.ortegacassou@gmail.com>
2020-06-16 00:54:56 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new Splide('.splide', {
+    type: 'loop',
+    // autoplay: true,
+    interval: 5000,
+    arrows: false,
+  }).mount();
+});
